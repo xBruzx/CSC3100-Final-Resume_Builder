991ad3b21c1d352c54c61afade5dd062e6b6b712
Alec Szczechowicz <65929878+xBruzx@users.noreply.github.com>
2026-04-27 20:33:09 +0000

    Changed tblJobs in database.sql to make sure it matched schema

--- db/database.sql
+++ db/database.sql
@@ -1,7 +1,8 @@
 CREATE TABLE tblJobs (
     job_id INTEGER PRIMARY KEY AUTOINCREMENT,
     company VARCHAR(256) NOT NULL,
-    start_date VARCHAR(256) NOT NULL,
-    end_date VARCHAR(256),
+    job_title VARCHAR(256) NOT NULL,
+    job_start_date VARCHAR(256) NOT NULL,
+    job_end_date VARCHAR(256),
     job_description VARCHAR(1000),
 );
