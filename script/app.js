f8c03cfde31eb0a5ec547737690cffdd1b90da75
Alec Szczechowicz <65929878+xBruzx@users.noreply.github.com>
2026-04-27 17:51:36 +0000

    Adding a new block of html and some js

---
+++ script/app.js
@@ -0,0 +1,6 @@
+
+
+document.querySelector("#btnGetStarted").addEventListener('click', () => {
+    document.querySelector("#divGetStarted").style = "display: none;"
+    document.querySelector("#divDashboard").style = "display: block;"
+})
