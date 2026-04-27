4dca4cd8f4f7a747bedddc61012a39c7c359ef72
Alec Szczechowicz <65929878+xBruzx@users.noreply.github.com>
2026-04-27 21:19:44 +0000

    Added buttons for navigation that work and some text as a place holder for new blocks

--- script/app.js
+++ script/app.js
@@ -4,3 +4,23 @@ document.querySelector("#btnGetStarted").addEventListener('click', () => {
     document.querySelector("#divGetStarted").style = "display: none;"
     document.querySelector("#divDashboard").style = "display: block;"
 })
+
+document.querySelector("#btnHome").addEventListener('click', () => {
+    document.querySelector("#divDashboard").style = "display: none;"
+    document.querySelector("#divGetStarted").style = "display: block;"
+})
+
+document.querySelector("#btnEditor").addEventListener('click', () => {
+    document.querySelector("#divEditor").style = "display: block;"
+    document.querySelector("#divDashboard").style = "display: none;"
+})
+
+document.querySelector("#btnGoHome").addEventListener('click', () => {
+    document.querySelector("#divGetStarted").style = "display: block;"
+    document.querySelector("#divEditor").style = "display: none;"
+})
+
+document.querySelector("#btnBackToDash").addEventListener('click', () => {
+    document.querySelector("#divDashboard").style = "display: block;"
+    document.querySelector("#divEditor").style = "display: none;"
+})
